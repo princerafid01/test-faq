af61267983713656d149bbd5cc89fe07886e6ca7
Mahmud Hasan Rafid <mahmudrafid02@gmail.com>
2023-12-23 14:21:44 +0600

    feat: Necessary Migration added

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
     }, [])
 
     return (
-            <Page>
+            <Page title="Home">
                 <Layout>
                     <Layout.Section>
                         <FormLayout>
